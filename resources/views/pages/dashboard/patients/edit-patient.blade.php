472bd0a0d8733a3e5d8277aa75dbdd4a3e83ab74
John Gualteros <gualterosjohn40@gmail.com>
2024-03-01 15:35:13 -0500

    feat: update patient

--- resources/views/pages/dashboard/patients/edit-patient.blade.php
+++ resources/views/pages/dashboard/patients/edit-patient.blade.php
@@ -8,59 +8,150 @@
 </a>
 <h2>Editar Paciente</h2>
 
-<form action="{{ route('updateUser', ['id' => $user->id, 'origin' => 'patients']) }}" method="POST">
+<div class="tabs">
+    <button id="client_tab" class="tab_button active" data-content="client_form">Cliente</button>
+    <button id="company_tab" class="tab_button" data-content="company_form">Entidad</button>
+</div>
+
+{{-- CLIENT FORM --}}
+<form action="{{ route('updateUser', ['id' => $user->id, 'origin' => 'patients']) }}" method="POST" id="client_form" class="form_tab active client_form">
     @csrf
     @method('PUT')
 
     <div class="form_group">
         <label for="name">Nombre</label>
-        <input type="text" id="name" name="name" required placeholder="Ingrese Nombre" value="{{ $user->name }}">
+        <input type="text" id="name" name="name" placeholder="Ingrese Nombre" value="{{ $user->name }}">
         <p class="error">{{ $errors->first('name') }}</p>
     </div>
 
+    {{-- LASTNAME --}}
     <div class="form_group">
         <label for="lastname">Apellido</label>
-        <input type="text" id="lastname" name="lastname" required placeholder="Ingrese su Apellido" value="{{ $user->lastname }}">
+        <input type="text" id="lastname" name="lastname" placeholder="Ingrese Apellido" value="{{ $user->lastname }}">
         <p class="error">{{ $errors->first('lastname') }}</p>
     </div>
 
-    {{-- type of document --}}
+    {{-- TYPE --}}
     <div class="form_group" >
-        <label for="type_document">Tipo de Documento</label>
-        <select name="type_document" id="type_document" required>
-            <option value="C.C" {{ $user->type_document == 'C.C' ? 'selected' : '' }}>Cédula de Ciudadanía</option>
-            <option value="Cédula de Extranjeria" {{ $user->type_document == 'Cédula de Extranjeria' ? 'selected' : '' }}>Cédula de Extranjería</option>
-            <option value="Pasaporte" {{ $user->type_document == 'Pasaporte' ? 'selected' : '' }}>Pasaporte</option>
+        <label for="type">Tipo</label>
+        <select name="type_document" id="type_document">
+            <option value="RC" {{ $user->type_document == 'RC' ? 'selected' : '' }}>Registro Civil</option>
+            <option value="TI" {{ $user->type_document == 'TI' ? 'selected' : '' }}>Tarjeta de identidad.</option>
+            <option value="CC" {{ $user->type_document == 'CC' ? 'selected' : '' }}>Cédula de Ciudadanía</option>
+            <option value="CE" {{ $user->type_document == 'CE' ? 'selected' : '' }}>Cédula de extranjería</option>
+            <option value="PT" {{ $user->type_document == 'PT' ? 'selected' : '' }}>Permiso por Protección Temporal</option>
+            <option value="CN" {{ $user->type_document == 'CN' ? 'selected' : '' }}>Certificado de nacido</option>
         </select>
         <p class="error">{{ $errors->first('type_document') }}</p>
     </div>
 
-    {{-- document number --}}
+    {{-- IDENTIFICATION --}}
     <div class="form_group">
-        <label for="document">Número de Documento</label>
-        <input type="text" id="document" name="document" required placeholder="Ingrese Número de Documento" value="{{ $user->document }}">
+        <label for="document">Identificación</label>
+        <input type="text" id="document" name="document" placeholder="Ingrese identificación" value="{{ $user->document }}">
         <p class="error">{{ $errors->first('document') }}</p>
     </div>
 
+    {{-- AGE --}}
     <div class="form_group">
         <label for="age">Edad</label>
-        <input type="number" id="age" name="age" required placeholder="Ingrese Edad" value="{{ $user->age }}">
+        <input type="text" id="age" name="age" placeholder="Ingrese Edad" value="{{ $user->age }}">
         <p class="error">{{ $errors->first('age') }}</p>
     </div>
 
-    {{-- gender --}}
+    {{-- SEX --}}
     <div class="form_group" >
         <label for="sex">Sexo</label>
-        <select name="sex" id="sex" required>
-            <option value="Hombre" {{ $user->sex == 'Hombre' ? 'selected' : '' }}>Hombre</option>
-            <option value="Mujer" {{ $user->sex == 'Mujer' ? 'selected' : '' }}>Mujer</option>
+        <select name="sex" id="sex">
+            <option value="M" {{ $user->sex == 'M' ? 'selected' : '' }}>Masculino</option>
+            <option value="F" {{ $user->sex == 'F' ? 'selected' : '' }}>Femenino</option>
         </select>
         <p class="error">{{ $errors->first('sex') }}</p>
     </div>
 
+    {{-- EMAIL --}}
     <div class="form_group">
         <label for="email">Correo Electrónico</label>
-        <input type="email" id="email" name="email" required placeholder="Ingrese Correo" value="{{ $user->email }}">
+        <input type="email" id="email" name="email" placeholder="Ingrese su Correo" value="{{ $user->email }}">
+        <p class="error">{{ $errors->first('email') }}</p>
+    </div>
+
+    <button type="submit">Actualizar</button>
+</form>
+
+{{-- COMPANY FORM --}}
+<form action="{{ route('updateUser', ['id' => $user->id, 'origin' => 'patients']) }}" method="POST" id="company_form" class="form_tab">
+    @csrf
+    @method('PUT')
+
+    <div class="form_group" >
+        <label for="company_id">Entidad</label>
+        <select name="company_id" id="company_id">
+            @foreach ($companies as $company)
+                <option value="{{ $company->id }}">{{ $company->name }}</option>
+            @endforeach
+        </select>
+        <p class="error">{{ $errors->first('company_id') }}</p>
+    </div>
+
+    @csrf
+    @method('PUT')
+
+    <div class="form_group">
+        <label for="name">Nombre</label>
+        <input type="text" id="name" name="name" placeholder="Ingrese Nombre" value="{{ $user->name }}">
+        <p class="error">{{ $errors->first('name') }}</p>
+    </div>
+
+    {{-- LASTNAME --}}
+    <div class="form_group">
+        <label for="lastname">Apellido</label>
+        <input type="text" id="lastname" name="lastname" placeholder="Ingrese Apellido" value="{{ $user->lastname }}">
+        <p class="error">{{ $errors->first('lastname') }}</p>
+    </div>
+
+    {{-- TYPE --}}
+    <div class="form_group" >
+        <label for="type">Tipo</label>
+        <select name="type_document" id="type_document">
+            <option value="RC" {{ $user->type_document == 'RC' ? 'selected' : '' }}>Registro Civil</option>
+            <option value="TI" {{ $user->type_document == 'TI' ? 'selected' : '' }}>Tarjeta de identidad.</option>
+            <option value="CC" {{ $user->type_document == 'CC' ? 'selected' : '' }}>Cédula de Ciudadanía</option>
+            <option value="CE" {{ $user->type_document == 'CE' ? 'selected' : '' }}>Cédula de extranjería</option>
+            <option value="PT" {{ $user->type_document == 'PT' ? 'selected' : '' }}>Permiso por Protección Temporal</option>
+            <option value="CN" {{ $user->type_document == 'CN' ? 'selected' : '' }}>Certificado de nacido</option>
+        </select>
+        <p class="error">{{ $errors->first('type_document') }}</p>
+    </div>
+
+    {{-- IDENTIFICATION --}}
+    <div class="form_group">
+        <label for="document">Identificación</label>
+        <input type="text" id="document" name="document" placeholder="Ingrese identificación" value="{{ $user->document }}">
+        <p class="error">{{ $errors->first('document') }}</p>
+    </div>
+
+    {{-- AGE --}}
+    <div class="form_group">
+        <label for="age">Edad</label>
+        <input type="text" id="age" name="age" placeholder="Ingrese Edad" value="{{ $user->age }}">
+        <p class="error">{{ $errors->first('age') }}</p>
+    </div>
+
+    {{-- SEX --}}
+    <div class="form_group" >
+        <label for="sex">Sexo</label>
+        <select name="sex" id="sex">
+            <option value="M" {{ $user->sex == 'M' ? 'selected' : '' }}>Masculino</option>
+            <option value="F" {{ $user->sex == 'F' ? 'selected' : '' }}>Femenino</option>
+        </select>
+        <p class="error">{{ $errors->first('sex') }}</p>
+    </div>
+
+    {{-- EMAIL --}}
+    <div class="form_group">
+        <label for="email">Correo Electrónico</label>
+        <input type="email" id="email" name="email" placeholder="Ingrese su Correo" value="{{ $user->email }}">
         <p class="error">{{ $errors->first('email') }}</p>
     </div>
 
